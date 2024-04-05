7b59242c69e842000bcdff2e026c441227f9f397
theomerman <edsonanime@hotmail.es>
2024-04-05 14:25:21 -0600

    terminals config files

---
+++ home/arch/.wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("CaskaydiaCove NFM")
+config.font_size = 20.0
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+-- and finally, return the configuration to wezterm
+return config
