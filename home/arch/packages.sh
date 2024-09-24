0d6bfe2a6b49c3561e16b0e07263dcca32869726
theomerman <57298571+theomerman@users.noreply.github.com>
2024-09-24 12:12:17 -0600

    Update packages.sh

--- home/arch/packages.sh
+++ home/arch/packages.sh
@@ -1,2 +1,2 @@
 ## install packages ##
-sudo pacman -S  xf86-video-amdgpu mesa vulkan-radeon plasma-meta git discord go networkmanager vim neovim wezterm zellij dolphin zsh docker luarocks lazygit fd ripgrep nodejs xclip npm python-neovim jdk-openjdk fastfetch wget ark zip python-pip htop openssh
+sudo pacman -S  xf86-video-amdgpu mesa vulkan-radeon plasma-meta git discord go networkmanager vim neovim wezterm zellij dolphin zsh docker luarocks lazygit fd ripgrep nodejs xclip npm python-neovim jdk-openjdk fastfetch wget ark zip python-pip htop openssh maven
