b7ee5b8ef69fb90e519c57b7a0f65b244d7339d9
theomerman <57298571+theomerman@users.noreply.github.com>
2024-10-01 10:26:54 -0600

    Update catpuccin-theme.lua

--- home/arch/.config/nvim/lua/plugins/catpuccin-theme.lua
+++ home/arch/.config/nvim/lua/plugins/catpuccin-theme.lua
@@ -1,12 +1,53 @@
+-- return {
+--   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+--   "catppuccin/nvim",
+
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "macchiato",
+--     })
+
+--     -- require("catppuccin").load()
+--   end,
+-- }
+
 return {
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  "catppuccin/nvim",
+  {
+    "LazyVim/LazyVim",
+    lazy = false,
+    priority = 1000,
 
-  config = function()
-    require("catppuccin").setup({
-      flavour = "macchiato",
-    })
-
-    -- require("catppuccin").load()
-  end,
+    opts = {
+      colorscheme = function()
+        -- require("catppuccin").load()
+      end,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      -- no_italic = true,
+      term_colors = true,
+      transparent_background = false,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
+    },
+  },
 }
